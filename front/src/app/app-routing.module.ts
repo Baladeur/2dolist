f4fbcf2a506fba1a8387814a8f626fa69456751f
Shimon S <shimonslit@gmail.com>
2024-02-19 11:16:45 +0100

    routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserPageComponent } from './user-page/user-page.component';
+//import { AuthGuard } from './auth.guard';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'user-page', component: UserPageComponent, /*canActivate: [AuthGuard]*/},
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
