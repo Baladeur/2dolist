cdb48bfb27a68aba14ca0fb259862ed61b33e7fd
arseniit <arseniit@gmail.com>
2024-02-20 01:43:52 +0100

    Impllemented 2 type of environment properties

---
+++ front/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://todolist-wcs-back.onrender.com',
+    frontUrl: 'https://twodolist1.onrender.com',
+    frontRegistrationComleteUrl: '/registration/complete/',
+};
